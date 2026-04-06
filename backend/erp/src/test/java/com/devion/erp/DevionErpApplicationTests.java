57f65b3c693a3130410bed0fbff793c160b5d92b
Devang Desai <1desaidevang@gmail.com>
2026-04-07 00:20:10 +0530

    Add files via upload

---
+++ backend/erp/src/test/java/com/devion/erp/DevionErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devion.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiLexErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
